b194164302e228bc5cf6baeefc0089fe93ab5b65
jum <puremented@gmail.com>
2022-10-12 21:20:43 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/index.jsx', './pages/projects.jsx'],
+  content: [
+    "./pages/index.jsx",
+    "./pages/projects.jsx",
+    "./pages/components/Icons.jsx",
+    "./pages/components/Main.jsx",
+    "./pages/components/Navbar.jsx",
+    "./pages/components/Project.jsx",
+    "./pages/_document.jsx",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
